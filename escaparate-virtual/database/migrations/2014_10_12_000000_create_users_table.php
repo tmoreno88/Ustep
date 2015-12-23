3463a394bd35b53b35453bf4af6329cb9f58aa04
Tamara <inidyu2@hotmail.com>
2015-12-23 21:21:30 +0100

    migrations with users, migrations and passResets tables

--- escaparate-virtual/database/migrations/2014_10_12_000000_create_users_table.php
+++ escaparate-virtual/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name',15);
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('slug')->default('');
             $table->rememberToken();
             $table->timestamps();
         });
