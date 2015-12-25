d9a199c44e5778284f785d6ca8a98d1859e24453
tamara <inidyu2@hotmail.com>
2015-12-25 03:47:09 +0100

    playing with fire... i'd said bootstrap and stylus

--- escaparate-virtual/gulpfile.js
+++ escaparate-virtual/gulpfile.js
@@ -1,16 +1,51 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var Task = elixir.Task;
+require('laravel-elixir-stylus');
+require('laravel-elixir-imagemin');
+require('laravel-elixir-modernizr');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.extend('speak', function(message){
+    new Task('speak', function(){
+        return gulp.src('').pipe(shell('say ' + message));
+    })
+    .watch('./app/**');
+});
+
+
+var paths = {
+    'bootstrap': './node_modules/bootstrap-styl',
+    'fontawesome': './node_modules/font-awesome-stylus'
+};
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.stylus('libs.styl')
+    .stylus('app.styl')
+    .browserify('libs.js')
+    .browserify('app.js')
+    .copy('./node_modules/select2/dist/css/select2.css', 'public/css')
+    .copy('./node_modules/select2-bootstrap-css/select2-bootstrap.css', 'public/css')
+    .copy('./node_modules/select2/dist/js/select2.full.js', 'public/js')
+    .modernizr()
+    .styles([
+        './public/css/libs.css',
+        './public/css/select2.css',
+        './public/css/select2-bootstrap.css',
+        './public/css/app.css'
+    ])
+   .scripts([
+        './public/js/vendor/modernizr-custom.js',
+         './public/js/libs.js',
+         './public/js/select2.full.js',
+         './public/js/app.js'
+    ])
+    .version(['css/all.css','js/all.js'])
+    .imagemin()
+    .copy(paths.fontawesome + '/fonts', 'public/build/fonts')
+    .copy(paths.bootstrap + '/fonts', 'public/build/fonts')
+    .browserSync({proxy: 'laravel.dev'})
+    .phpUnit()
+    .phpSpec()
+    .speak('File Changed');
 });
